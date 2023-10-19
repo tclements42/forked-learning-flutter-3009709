58611e9f480c8f14eb3ac73bf8474f07130d7fe9
Travis Clements <travis.clements@alkami.com>
2023-10-18 23:18:51 -0400

    Final changes for 03_02

---
+++ chat_app/lib/login_page.dart
@@ -0,0 +1,57 @@
+// TODO: Move LoginPage code to it's own file
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Button clicked');
+        },
+      ),
+      //TODO: Add the text and image from the design
+      body: Column(
+        children: [
+          Text(
+            'Let\'s sign you in',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.brown,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 0.5,
+            ),
+          ),
+          Text(
+            'Welcome back! \n You\'ve been missed!',
+            style: TextStyle(
+                fontWeight: FontWeight.bold, fontSize: 20, color: Colors.blue),
+          ),
+          Image.network(
+            'https://freepngimg.com/thumb/bart_simpson/67747-homer-maggie-bart-marge-lisa-simpson.png',
+            height: 200,
+            width: 200,
+          ),
+          Container(
+            height: 150,
+            width: 150,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(50),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.fitHeight,
+                  image: NetworkImage(
+                      'https://freepngimg.com/thumb/bart_simpson/67747-homer-maggie-bart-marge-lisa-simpson.png')),
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(24),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
